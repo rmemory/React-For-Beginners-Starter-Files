3256ceb851e37e036d2b2c8e7c62793dec95b158
Richard Memory <richardfmemory@gmail.com>
2018-04-17 13:36:55 -0600

    Added links to the already compiled css

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,40 +1,12 @@
 import React from 'react';
 
-/*
- * We could have done this:
- * import React, {Fragment} from 'react';
- */
-
 class StorePicker extends React.Component {
-	// All React.Components must implement render
 	render() {
-		/* We could return this, 
-		 * 
-		 * React.createElement('p', { className: 'store-selector'}, 'heyyooo');
-		 * But don't use React.createElement.
-		 */
-
 		return (
 			<form className="store-selector">
-			{/* This is a comment */}
 				<h2>Please Enter A Store</h2>
 			</form>
 		)
-
-		/* 
-		 * Note, a render method can only return a single root HTML element. But, new in React 16.2, we can do this:
-		 * 
-		 *	render() {
-			return (
-			<React.Fragment>
-				<p>Fish!</p>
-				<form className="store-selector">
-					<h2>Please Enter a store</h2>
-				</form>
-			</React.Fragment>
-			)
-			}
-		 */
 	}
 }
 
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,17 +1,7 @@
-/* 
- * Import the base react module. See package.json
- * for the react version used. 
- * 
- * Could also do this,
- * 
- * import React, {Component} from 'react';
- */
 import React from 'react';
-
-/* The react-dom module is what connects us to a web
-   page, as opposed to a mobile (native) app */
 import {render} from 'react-dom';
 import StorePicker from './components/StorePicker.js';
+import "./css/style.css";
 
 /* Main mounting point. See public/index.html */
 render(<StorePicker/>, document.querySelector('#main'));
