d84103b770629f6932af65b3cb4905af795d0cca
Richard Memory <richardfmemory@gmail.com>
2018-04-17 14:04:53 -0600

    Create initial application layout

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import StorePicker from './components/StorePicker.js';
 import "./css/style.css";
+import App from './components/App.js';
 
 /* Main mounting point. See public/index.html */
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
