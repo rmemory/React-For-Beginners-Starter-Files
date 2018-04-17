68cd3933553e42bc0b18a6644634c83e72714a8b
Richard Memory <richardfmemory@gmail.com>
2018-04-17 13:18:10 -0600

    Add first StorePicker.js

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+	// All React.Components must implement render
+	render() {
+		return <p>Heyyyy</p>
+	}
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,17 @@
-// let's go!
+/* 
+ * Import the base react module. See package.json
+ * for the react version used. 
+ * 
+ * Could also do this,
+ * 
+ * import React, {Component} from 'react';
+ */
+import React from 'react';
+
+/* The react-dom module is what connects us to a web
+   page, as opposed to a mobile (native) app */
+import {render} from 'react-dom';
+import StorePicker from './components/StorePicker.js';
+
+/* Main mounting point. See public/index.html */
+render(<StorePicker/>, document.querySelector('#main'));
