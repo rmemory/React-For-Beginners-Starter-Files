d1cfb2570430d87c9a243966123761b585365a15
Richard Memory <richardfmemory@gmail.com>
2018-04-17 14:41:37 -0600

    Modify Header.js to become a stateless functional component

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-
-/* At the moment, this causes a not used warning because its not used
-   but it soon will be, so its left in as-is */
 import StorePicker from './components/StorePicker.js';
 import "./css/style.css";
 import App from './components/App.js';
